1eb5a47795969b6dda470e367990d9c7a8e60f7b
Cian O'Gralaigh <ihatecian@gmail.com>
2020-12-29 19:10:59 +0000

    config setup

--- src/components/congrats/congrats.test.tsx
+++ src/components/congrats/congrats.test.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { shallow } from 'enzyme';
 import { Congrats } from './congrats';
 import { findByTestAttr } from '../../../test/test-utils';
-
-Enzyme.configure({ adapter: new Adapter() });
 
 const setup = (props: { success: boolean }) => shallow(<Congrats {...props} />);
 
@@ -17,6 +14,12 @@ test('renders no text when success prop is false', () => {
   const wrapper = setup({ success: false });
   const text = findByTestAttr(wrapper, 'congrats').text();
   expect(text).toBe('');
+});
+
+test('renders non-empty message when success prop is true', () => {
+  const wrapper = setup({ success: true });
+  const text = findByTestAttr(wrapper, 'congrats').text();
+  expect(text).toBe('Congrats');
 });
 
 test('renders non-empty message when success prop is true', () => {
