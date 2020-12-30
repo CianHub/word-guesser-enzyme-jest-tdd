6c5a0ba5f39bc4ed434202a4556d34c9975ad2a8
Cian O'Gralaigh <ihatecian@gmail.com>
2020-12-30 17:35:59 +0000

    Add guessed words component and tests

--- src/components/guessedWords/GuessedWords.test.tsx
+++ src/components/guessedWords/GuessedWords.test.tsx
@@ -6,9 +6,50 @@ import { findByTestAttr } from '../../../test/test-utils';
 const setup = (props: { guessedWords: GuessedWord[] }) =>
   shallow(<GuessedWords {...props} />);
 
-test('it should render', () => {
-  const comp = setup({
-    guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+describe('If no words guessed,', () => {
+  test('it should render', () => {
+    const wrapper = setup({
+      guessedWords: [],
+    });
+    const comp = findByTestAttr(wrapper, 'guessed-words');
+    expect(comp.length).toBe(1);
   });
-  expect(comp).toBeTruthy();
+
+  test('it should render instructions', () => {
+    const wrapper = setup({
+      guessedWords: [],
+    });
+    const comp = findByTestAttr(wrapper, 'instructions');
+    expect(comp.length).toBe(1);
+  });
+});
+
+describe('If  words guessed,', () => {
+  test('it should render', () => {
+    const wrapper = setup({
+      guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+    });
+    const comp = findByTestAttr(wrapper, 'guessed-words');
+    expect(comp.length).toBe(1);
+  });
+
+  test('it should render guessed word section', () => {
+    const wrapper = setup({
+      guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+    });
+    const comp = findByTestAttr(wrapper, 'guessed-words-section');
+    expect(comp.length).toBe(1);
+  });
+
+  test('it should render display correct number of words', () => {
+    const wrapper = setup({
+      guessedWords: [
+        { guessedWord: 'train', letterMatchCount: 3 },
+        { guessedWord: 'agile', letterMatchCount: 1 },
+        { guessedWord: 'party', letterMatchCount: 5 },
+      ],
+    });
+    const comp = findByTestAttr(wrapper, 'guessed-word');
+    expect(comp.length).toBe(3);
+  });
 });
--- src/components/guessedWords/GuessedWords.tsx
+++ src/components/guessedWords/GuessedWords.tsx
@@ -10,5 +10,34 @@ interface Props {
 }
 
 export const GuessedWords: React.FC<Props> = ({ guessedWords }) => {
-  return <div data-test="component-guessed-words"></div>;
+  let contents: JSX.Element = (
+    <span data-test="component-instructions">
+      Try to guess the secret words
+    </span>
+  );
+
+  if (guessedWords.length > 0) {
+    contents = (
+      <div data-test="component-guessed-words-section">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWords.map((guessedWord: GuessedWord, index: number) => (
+              <tr key={index} data-test="component-guessed-word">
+                <td>{guessedWord.guessedWord}</td>
+                <td>{guessedWord.letterMatchCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
