8f9ab513f3724b70ffc13ed3411e9562e54ea47c
Cian O'Gralaigh <ihatecian@gmail.com>
2021-01-02 13:33:20 +0000

    Add unit testing and logic to reducer

---
+++ src/reducer/successReducer.test.tsx
@@ -0,0 +1,14 @@
+import { actionTypes } from '../actions';
+import { successReducer } from './successReducer';
+
+test('returns initial state of false if no action passed', () => {
+  const newState = successReducer();
+  expect(newState).toBeFalsy();
+});
+
+test('returns state of true if CORRECT_GUESS action passed', () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS,
+  });
+  expect(newState).toBeTruthy();
+});
--- src/reducer/successReducer.tsx
+++ src/reducer/successReducer.tsx
@@ -1,6 +1,13 @@
+import { actionTypes, Action } from '../actions';
+
 export const successReducer = (
-  state: string[],
-  action: { type: string; payload: boolean }
-) => {
-  return null;
+  state: boolean = false,
+  action?: Action
+): boolean => {
+  switch (action?.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
