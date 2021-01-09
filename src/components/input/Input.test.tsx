39dd0e1a709f8bff339eef4a44c065084b8ac471
Cian O'Gralaigh <ihatecian@gmail.com>
2021-01-09 19:54:24 +0000

    Add functionality and tests for input component

--- src/components/input/Input.test.tsx
+++ src/components/input/Input.test.tsx
@@ -2,11 +2,12 @@ import Input from './Input';
 import { findByTestAttr } from '../../../test/test-utils';
 import { shallow, ShallowWrapper } from 'enzyme';
 
-const setup = (): ShallowWrapper<
-  any,
-  Readonly<{}>,
-  React.Component<{}, {}, any>
-> => shallow(<Input />);
+import React from 'react';
+
+const setup = (
+  secretWord = 'party'
+): ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>> =>
+  shallow(<Input secretWord={secretWord} />);
 
 describe('Input', () => {
   let wrapper: ShallowWrapper<any, Readonly<{}>, React.Component<{}, {}, any>>;
@@ -18,5 +19,34 @@ describe('Input', () => {
   it('should render', () => {
     const component = findByTestAttr(wrapper, 'input');
     expect(component.length).toBe(1);
+  });
+});
+
+describe('state controlled input element', () => {
+  test('state updates with value of input upon change', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    let wrapper = setup();
+    const input = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    input.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+
+  test('prevent default and state updates are called onsubmit', () => {
+    const mockSetCurrentGuess = jest.fn();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    let wrapper = setup();
+    const btn = findByTestAttr(wrapper, 'submit-button');
+
+    const mockEvent = { preventDefault: jest.fn() };
+    btn.simulate('click', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
+    expect(mockEvent.preventDefault).toHaveBeenCalled();
   });
 });
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,27 +1,46 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-interface Props {}
+interface Props {
+  secretWord: string;
+}
 
-const Input: React.FC<Props> = () => {
-  const contents = (
-    <form className="form-inline">
-      <input
-        data-test="component-input-element"
-        className="mb-2 mx-sm-3"
-        type="text"
-        placeholder="Guess a word"
-      />
-      <button
-        type="submit"
-        data-test="component-submit-button"
-        className="btn btn-primary mb-2"
-      >
-        Submit
-      </button>
-    </form>
+const Input: React.FC<Props> = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    setCurrentGuess('');
+
+    //TODO update geussedWords context
+    //TODO check if word === secretWord and update success context if so
+  };
+
+  return (
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input
+          value={currentGuess}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setCurrentGuess(e.target.value)
+          }
+          data-test="component-input-box"
+          className="mb-2 mx-sm-3"
+          type="text"
+          placeholder="Guess a word"
+        />
+        <button
+          type="submit"
+          data-test="component-submit-button"
+          className="btn btn-primary mb-2"
+          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            handleSubmit(e)
+          }
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
-
-  return <div data-test="component-input">{contents}</div>;
 };
 
 export default Input;
